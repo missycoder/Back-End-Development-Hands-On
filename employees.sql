bcda275ca502bc27d59473e1ed1c62cbd885e67b
missycoder mylan <noreen.taha@outlook.com>
2024-03-26 07:10:16 +0000

    create FK, and insert rows into dept

--- employees.sql
+++ employees.sql
@@ -18,3 +18,14 @@ date_of_employment DATE NOT NULL,
 -- foreign key
 department_id INT UNSIGNED NOT NULL
 ) ENGINE = innodb;
+
+-- CREATE foreign key such that employees.department_id
+-- refers to departments.department_id
+ALTER TABLE employees ADD CONSTRAINT fk_employees_departments
+FOREIGN KEY (department_id) REFERENCES departments (department_id);
+
+-- INSERT rows into department
+INSERT INTO departments (name) VALUE ('Accounting');
+
+INSERT INTO departments(name) VALUE ('Marketing'),('Operations');
+
