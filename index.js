948fc744a6a030db4c99067a6399127ff564412b
missycoder mylan <noreen.taha@outlook.com>
2024-03-27 19:28:02 +0000

    draft

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const hbs = require('hbs');
 const wax = require('wax-on');
-require ('dotenv').config();
+// const { createConnection } = require('mysql2/promise');
+require('dotenv').config();
 
 const app = express();
+
 // set up view engine
 app.set('view engine', 'hbs');
+
+
+// require('handlebars-helpers')({
+//     handlebars: hbs.handlebars
+// })
 
 // enable static files
 app.use(express.static('public'));
 
+app.use(express.urlencoded({
+    extended: false
+}))
+
 // wax-on (template inheritance)
 wax.on(hbs.handlebars);
-wax.setLayoutPath('../views/layouts');
+wax.setLayoutPath('./views/layouts');
 
-app.get('/', function(req,res){
-    res.send("hello world")
-});
+async function main() {
+    const connection = await createConnection({
+        'host': process.env.DB_HOST,
+        'user': process.env.DB_USER,
+        'database': process.env.DB_DATABASE,
+        'password': process.env.DB_PASSWORD
+    })
 
-app.listen(3000, function(){
+
+app.listen(3000, function() {
     console.log("server has started");
 })
+}
+
+main();
